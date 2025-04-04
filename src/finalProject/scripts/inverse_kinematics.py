d333dceb2459c408cd1a0f1efcc1195c66e488a8
JB55Matthews <blackhedley25@gmail.com>
2025-04-04 19:52:33 -0230

    working inverse kin along with forward

--- src/finalProject/scripts/inverse_kinematics.py
+++ src/finalProject/scripts/inverse_kinematics.py
@@ -1,30 +1,53 @@
 #!/usr/bin/env python3
 import rospy
 import numpy as np
-from geometry_msgs.msg import Pose, PoseStamped
 from sensor_msgs.msg import JointState
-# import modern_robotics as mr
+from topic_tools.srv import MuxSelect, MuxSelectRequest
+import modern_robotics as mr
+#---------------------------------------------------------
+# Note: RViz link 8 orientation and position will be 
+#       correct, but expand window to see e-0x as will not show
+#       exact positions if 0. See this by uncommenting zero position.
+#
+# User input position
+position = np.array([0.3157, 0.0, 0.6571])
+# Zero postion
+# position = np.array([0, 0, 0.91])
 
-L1 = 550
-L2 = 300
-L3 = 60
+# User input rotation matrix
+orientation = np.array([[0, 0, -1],
+                        [0, 1, 0],
+                        [1, 0, 0]])
+# Zero orientation
+# orientation = np.array([[1, 0, 0],
+#                         [0, 1, 0],
+#                         [0, 0, 1]])
+
+# or give orientation as Quaternion. If so, comment out above matrix
+# orientation = rotationFromQuaternion(x, y, z, w)
+
+# make sure to "source devel/setup.bash"
+# then simply "rosrun finalProject inverse_kinematcs.py"
+#------------------------------------------------------
+
+L1 = 0.55
+L2 = 0.3
+L3 = 0.06
 
 ew = 0.001
 ev = 0.0001
 
-position = np.array([0.3157, 0.0, 0.6571])
-orientation = np.array([[0, 0, -1],
-                        [0, 1, 0],
-                        [1, 0, 0]])
-
-printed_count = 0
+def rotationFromQuaternion(x, y, z, w):
+    return [[1-2*y**2-2*z**2, 2*x*y-2*w*z, 2*x*z+2*w*y],
+     [2*y*x+2*z*w, 1-2*x**2-2*z**2, 2*z*y-2*x*w],
+     [2*x*z-2*w*y, 2*z*y+2*x*w, 1-2*x**2-2*y**2]]
 
 def inverse_kinematics(position, orientation):
-    theta = np.array([0, np.pi/6, np.pi/6, np.pi/6, 0, np.pi/6, 0])
+    theta = np.array([0, np.pi/6, np.pi/6, np.pi/6, 0, np.pi/6, np.pi/6])
     M = np.array([[1, 0, 0, 0],
-                 [0, 1, 0, 0],
-                 [0,0,0, L1 + L2 + L3],
-                 [0, 0, 0, 1]])
+                  [0, 1, 0, 0],
+                  [0,0,1, L1 + L2 + L3],
+                  [0, 0, 0, 1]])
 
     B1 = np.array([0, 0, 1, 0, 0, 0])
     B2 = np.array([0, 1, 0, L1 + L2 + L3, 0, 0])
@@ -44,19 +67,37 @@ def inverse_kinematics(position, orientation):
     print("Joint Angles: ", joint_angles) 
     return joint_angles[0]
 
-if __name__ == "__main__":
-    rospy.init_node("ik_solver_node", anonymous=False)
+def control_arm():
+    rospy.init_node('script_joint_publisher')
+    pub = rospy.Publisher('/mux/script', JointState, queue_size=10)
+    rate = rospy.Rate(10)  # 10Hz
 
-    joint_pub = rospy.Publisher("/joint_states", JointState, queue_size=10)
+    # Switch mux to script input
+    rospy.wait_for_service('/mux/select')
+    mux_select = rospy.ServiceProxy('/mux/select', MuxSelect, persistent=True)
+    mux_select('/mux/script')  # Take control
 
     joint_positions = inverse_kinematics(position, orientation)
 
-    joint_msg = JointState()
-    joint_msg.name = [f"joint{i+1}" for i in range(len(joint_positions))]
+    try:
+        while not rospy.is_shutdown():
+            # Create and publish your JointState message
+            # joint_positions = inverse_kinematics(position, orientation)
+            joint_msg = JointState()
+            joint_msg.header.stamp = rospy.Time.now()
+            joint_msg.name = [f"joint{i+1}" for i in range(len(joint_positions))]
+            joint_msg.position = joint_positions  # Your target positions
+            pub.publish(joint_msg)
+            rate.sleep()
+    finally:
+        # Ensure mux switches back to GUI input even on exception
+        mux_select_request = MuxSelectRequest()
+        mux_select_request.topic = "/mux/gui"
+        mux_select = rospy.ServiceProxy('/mux/select', MuxSelect, persistent=True)
+        mux_select(mux_select_request)  # Release control
 
-    joint_msg.header.stamp = rospy.Time.now()
-    joint_msg.position = joint_positions
-    joint_pub.publish(joint_msg)
-    print("Publishing joint states...", joint_msg)
-
-    rospy.spin()
+if __name__ == "__main__":
+    try:
+        control_arm()
+    except rospy.ROSInterruptException:
+        pass
